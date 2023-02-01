3dcaec94260d6991a8047275344d1f5852822957
LeighAnnLavigne <lamcompetente@gmail.com>
2023-02-02 03:05:24 +0800

    Home activity to be the default activity without design

---
+++ app/src/main/java/com/example/it193project/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.it193project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button btn_Admin = findViewById(R.id.buttonAdmin);
+        Button btn_User = findViewById(R.id.buttonUser);
+
+        //ADMIN -> MainActivity
+        btn_Admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
